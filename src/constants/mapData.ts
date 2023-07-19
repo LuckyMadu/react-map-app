d06bcc3e3552eab3a599e67f2fbd2f1baf30ebd1
Lahiru <lahirua@swivelgroup.com.au>
2023-07-19 21:41:22 +0530

    enable vercel cors

---
+++ src/constants/mapData.ts
@@ -0,0 +1,235 @@
+export const mapData = {
+  predictions: [
+    {
+      description:
+        "Malaysian Dining Delights, Vellgrove Avenue, Parkwood WA, Australia",
+      matched_substrings: [
+        {
+          length: 8,
+          offset: 0,
+        },
+      ],
+      place_id: "ChIJO9vuTkO8MioRT-87cPtr7I0",
+      reference: "ChIJO9vuTkO8MioRT-87cPtr7I0",
+      structured_formatting: {
+        main_text: "Malaysian Dining Delights",
+        main_text_matched_substrings: [
+          {
+            length: 8,
+            offset: 0,
+          },
+        ],
+        secondary_text: "Vellgrove Avenue, Parkwood WA, Australia",
+      },
+      terms: [
+        {
+          offset: 0,
+          value: "Malaysian Dining Delights",
+        },
+        {
+          offset: 27,
+          value: "Vellgrove Avenue",
+        },
+        {
+          offset: 45,
+          value: "Parkwood",
+        },
+        {
+          offset: 54,
+          value: "WA",
+        },
+        {
+          offset: 58,
+          value: "Australia",
+        },
+      ],
+      types: ["restaurant", "food", "point_of_interest", "establishment"],
+    },
+    {
+      description:
+        "Malaysian Laksa House, Elizabeth Street, Melbourne VIC, Australia",
+      matched_substrings: [
+        {
+          length: 8,
+          offset: 0,
+        },
+      ],
+      place_id: "ChIJk2GSxDRd1moRexUoozqwG7w",
+      reference: "ChIJk2GSxDRd1moRexUoozqwG7w",
+      structured_formatting: {
+        main_text: "Malaysian Laksa House",
+        main_text_matched_substrings: [
+          {
+            length: 8,
+            offset: 0,
+          },
+        ],
+        secondary_text: "Elizabeth Street, Melbourne VIC, Australia",
+      },
+      terms: [
+        {
+          offset: 0,
+          value: "Malaysian Laksa House",
+        },
+        {
+          offset: 23,
+          value: "Elizabeth Street",
+        },
+        {
+          offset: 41,
+          value: "Melbourne",
+        },
+        {
+          offset: 51,
+          value: "VIC",
+        },
+        {
+          offset: 56,
+          value: "Australia",
+        },
+      ],
+      types: ["restaurant", "food", "point_of_interest", "establishment"],
+    },
+    {
+      description:
+        "Malaysian Kampung Restaurant, Centre Road, Bentleigh VIC, Australia",
+      matched_substrings: [
+        {
+          length: 8,
+          offset: 0,
+        },
+      ],
+      place_id: "ChIJi9VJdlxp1moRo25P0CnPlLo",
+      reference: "ChIJi9VJdlxp1moRo25P0CnPlLo",
+      structured_formatting: {
+        main_text: "Malaysian Kampung Restaurant",
+        main_text_matched_substrings: [
+          {
+            length: 8,
+            offset: 0,
+          },
+        ],
+        secondary_text: "Centre Road, Bentleigh VIC, Australia",
+      },
+      terms: [
+        {
+          offset: 0,
+          value: "Malaysian Kampung Restaurant",
+        },
+        {
+          offset: 30,
+          value: "Centre Road",
+        },
+        {
+          offset: 43,
+          value: "Bentleigh",
+        },
+        {
+          offset: 53,
+          value: "VIC",
+        },
+        {
+          offset: 58,
+          value: "Australia",
+        },
+      ],
+      types: ["restaurant", "food", "point_of_interest", "establishment"],
+    },
+    {
+      description: "Malaysia Grill, West 104th Street, New York, NY, USA",
+      matched_substrings: [
+        {
+          length: 8,
+          offset: 0,
+        },
+      ],
+      place_id: "ChIJC07YUCT2wokRBgm4re83qG8",
+      reference: "ChIJC07YUCT2wokRBgm4re83qG8",
+      structured_formatting: {
+        main_text: "Malaysia Grill",
+        main_text_matched_substrings: [
+          {
+            length: 8,
+            offset: 0,
+          },
+        ],
+        secondary_text: "West 104th Street, New York, NY, USA",
+      },
+      terms: [
+        {
+          offset: 0,
+          value: "Malaysia Grill",
+        },
+        {
+          offset: 16,
+          value: "West 104th Street",
+        },
+        {
+          offset: 35,
+          value: "New York",
+        },
+        {
+          offset: 45,
+          value: "NY",
+        },
+        {
+          offset: 49,
+          value: "USA",
+        },
+      ],
+      types: [
+        "meal_delivery",
+        "restaurant",
+        "food",
+        "point_of_interest",
+        "establishment",
+      ],
+    },
+    {
+      description:
+        "Malaysian Kopitiam, Westfields Boulevard, Centreville, VA, USA",
+      matched_substrings: [
+        {
+          length: 8,
+          offset: 0,
+        },
+      ],
+      place_id: "ChIJoXpaJkBEtokRdrfx22oqR_I",
+      reference: "ChIJoXpaJkBEtokRdrfx22oqR_I",
+      structured_formatting: {
+        main_text: "Malaysian Kopitiam",
+        main_text_matched_substrings: [
+          {
+            length: 8,
+            offset: 0,
+          },
+        ],
+        secondary_text: "Westfields Boulevard, Centreville, VA, USA",
+      },
+      terms: [
+        {
+          offset: 0,
+          value: "Malaysian Kopitiam",
+        },
+        {
+          offset: 20,
+          value: "Westfields Boulevard",
+        },
+        {
+          offset: 42,
+          value: "Centreville",
+        },
+        {
+          offset: 55,
+          value: "VA",
+        },
+        {
+          offset: 59,
+          value: "USA",
+        },
+      ],
+      types: ["restaurant", "food", "point_of_interest", "establishment"],
+    },
+  ],
+  status: "OK",
+};
